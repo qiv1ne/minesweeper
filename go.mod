5da4e198a5eecbd97ca910f8f28cf788da720ace
qiv1ne <sergeivanov827@gmail.com>
2025-01-26 22:30:02 +0300

    create alias for [][]Cell, include my logger, have bug: has some change to generate empty board

--- go.mod
+++ go.mod
@@ -1,11 +1,5 @@
 module github.com/qiv1ne/minesweeper
 
-go 1.23.4
+go 1.23.5
 
-require github.com/rs/zerolog v1.33.0
-
-require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-)
+require github.com/qiv1ne/log v0.0.0-20250126162742-d628fb843829
--- minesweeper.go
+++ minesweeper.go
@@ -1,34 +1,54 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 
-	"github.com/rs/zerolog/log"
+	"github.com/qiv1ne/log"
+)
+
+var (
+	logger, _ = log.New(os.Stdin, &log.Opts{
+		FuncName:   true,
+		FileName:   true,
+		LineNumber: true,
+		Date:       false,
+		Time:       false,
+	})
 )
 
 // Represent a one minesweeper board cell.
 type Cell struct {
-	Empty       bool // Cell is empty when it don't contain the mine or number.
-	MinesAround int  // Count of the mines around of this cell. If it 0 the Empty field should be true.
+	// Cell is empty when it don't contain the mine or number.
+	Empty bool
+
+	// Count of the mines around of this cell. If it 0 the Empty field should be true.
+	MinesAround int
 
 	// Flagged field represent is cell flagged of not.
-	// I think this field have a right to exists. For example if in the future I will want to add hints in the game.
+	// I think this field have a right to exists.
+	// For example if in the future I will want to add hints in the game.
 	Flagged bool
 
 	IsMine   bool // If the cell contain mine it true.
 	Revealed bool // If user reveal this cell it true.
 }
 
+// Board is alias for [][]Cell type. I create it for linking functions to type.
+type Board [][]Cell
+
 // MineBoard struct defines game map.
 type MineBoard struct {
 	BoardConfig
 
-	// RealBoard represent the game board with mines.
-	RealBoard [][]Cell
-	// UserBoard represent user's board with flags and not revealed mines.
-	UserBoard [][]Cell
+	// Real represent the game board with mines.
+	Real Board
+	// User represent user's board with flags and not revealed mines.
+	User Board
 	// MinesRemain represent how many unflaged mines on the user board.
 	MinesRemain int
 }
@@ -45,44 +65,55 @@ func NewSeed() int64 {
 	return rand.New(rand.NewSource(time.Now().Unix())).Int63()
 }
 
-// The placeMines function accept *MineBoard pointer and place mines in the RealBoard.
+// The placeMines function place mines in Board.
 // Like params function accept 1D board(you can use To1D() function) and count of mines
 // It panic if error occurred.
-func placeMines(oneDboard []Cell, minesCount int, seed int64) ([]Cell, error) {
-	log.Info().Str("mines count", fmt.Sprintf("%d", minesCount)).Msg("Placing mines to board.")
-
-	if len(oneDboard) < minesCount {
-		return nil, fmt.Errorf("Board of size %d can't contain %d mines", len(oneDboard), minesCount)
-	}
+func (b Board) placeMines(minesCount int, seed int64) error {
+	logger.Print(log.Info("Placing mines"))
 	if seed <= 0 {
-		return nil, fmt.Errorf("Seed can't be <= 0. Use NewSeed() function to generate new seed.", len(oneDboard), minesCount)
+		err := errors.New("Seed can't be <= 0. Use NewSeed() function to generate new seed.")
+		logger.Print(log.Error(err))
+		return err
 	}
-
-	var minesPlaced int
+	logger.Print(log.Info("check count of mines"), log.Str(strconv.Itoa(len(b)), strconv.Itoa(len(b[0]))))
+	if len(b)*len(b[0]) <= minesCount {
+		err := errors.New("Board can't contain mines more than it length")
+		logger.Print(log.Error(err))
+		return err
+	}
 
 	// Create random generator with given seed.
 	r := rand.New(rand.NewSource(seed))
 
-	for minesPlaced < minesCount {
-		// Transform board to 1D for using the length of it as limit for random function
-		// Randomize the coordinate of the mine.
-		x := r.Intn(len(oneDboard))
-
-		if !oneDboard[x].IsMine {
-			oneDboard[x].IsMine = true
-			minesPlaced++
+	mines := make(map[int]struct{}, minesCount)
+	for len(mines) < minesCount {
+		rand := r.Intn(len(b) * len(b[0]))
+		if _, ok := mines[rand]; !ok {
+			mines[rand] = struct{}{}
 		}
 	}
-	log.Info().
-		Str("mines count", fmt.Sprintf("%d", minesCount)).
-		Msg("Successfully place all mines on the board.")
 
-	return oneDboard, nil
+	logger.Print(log.Info("Mines calculated"),
+		log.Str("placed mines", fmt.Sprintf("%v", mines)))
+	var c int
+	for i := range b {
+		for j := range b[i] {
+			// logger.Print(log.Info("Check cell"), log.Str("ok", fmt.Sprintf("%v", mines[c])))
+			if _, ok := mines[c]; ok {
+				b[i][j].IsMine = true
+				c++
+				logger.Print(log.Info("Mine placed"),
+					log.Str("row", fmt.Sprintf("%d", i)),
+					log.Str("column", fmt.Sprintf("%d", j)))
+			}
+		}
+	}
+	return nil
 }
 
 // The PrintBroadGracefully is debug function to see how the board look.
-func PrintBroadGracefully(board [][]Cell) {
-	for _, row := range board {
+func (b Board) Print() {
+	for _, row := range b {
 		for _, cell := range row {
 			if !cell.Revealed {
 				fmt.Print("■ ")
@@ -112,48 +143,41 @@ func PrintBroadGracefully(board [][]Cell) {
 
 // NewMineBoard function create filled MineBoard struct
 func NewMineBoard(opts BoardConfig) (*MineBoard, error) {
-	board, err := createBoard(opts)
+	user, err := createBoard(opts)
 	if err != nil {
 		return nil, err
 	}
-	realBoard, err := createBoard(opts)
-	realBoard, err = RevealAll(realBoard)
+	real, err := createBoard(opts)
+	if err != nil {
+		return nil, err
+	}
+	err = real.RevealAll()
 	if err != nil {
 		return nil, err
 	}
 	return &MineBoard{
 		BoardConfig: opts,
-		RealBoard:   realBoard,
-		UserBoard:   board,
+		Real:        real,
+		User:        user,
 		MinesRemain: opts.Mines,
 	}, nil
 }
 
 // The CreateEmptyBoard create empty matrix of Cell.
-func createBoard(opts BoardConfig) ([][]Cell, error) {
-	log.Info().
-		Str("Config", fmt.Sprintf("%v", opts)).
-		Msg("Creating empty board.")
+func createBoard(opts BoardConfig) (Board, error) {
+	logger.Print(log.Info("creating new board"))
 	// Creating empty board
-	board := make([][]Cell, opts.Height)
+	board := make(Board, opts.Height)
 	for i := range board {
 		board[i] = make([]Cell, opts.Width)
 	}
 
 	if opts.Mines != 0 {
-		board1d, err := To1D(board)
-		if err != nil {
-			return [][]Cell{}, err
-		}
-		boardWithMines, err := placeMines(board1d, opts.Mines, opts.Seed)
+		err := board.placeMines(opts.Mines, opts.Seed)
 		if err != nil {
 			return board, err
 		}
-		board, err = To2D(boardWithMines, opts.Height, opts.Width)
-		if err != nil {
-			return board, err
-		}
-		board, err = PlaceNumbers(board)
+		err = board.placeNumbers()
 		if err != nil {
 			return board, err
 		}
@@ -165,95 +189,37 @@ func createBoard(opts BoardConfig) ([][]Cell, error) {
 // First purpose of using the 1D board is to put a mines simpler.
 // Function accept the 2D board and return 1D board.
 // If the error occurred due the process of a board function is panic.
-func To1D(board2D [][]Cell) ([]Cell, error) {
-	log.Info().Msg("Converting 2D board to 1D")
-	if len(board2D) == 0 {
-		return make([]Cell, 0), fmt.Errorf("board is empty(len() = %d)", len(board2D))
+func (b Board) To1D() ([]Cell, error) {
+	logger.Print(log.Info("Converting 2D board to 1D"))
+	if len(b) == 0 {
+		return make([]Cell, 0), errors.New("board is empty")
 	}
 	// Create result 1D board with capacity of left side * top side
-	board1D := make([]Cell, 0)
+	board1D := make([]Cell, len(b)*len(b[0]))
 
-	for i := range board2D {
-		for j := range board2D[i] {
-			board1D = append(board1D, board2D[i][j])
+	for i := range b {
+		for j := range b[i] {
+			board1D[i+j] = b[i][j]
 		}
 	}
 	return board1D, nil
 }
 
-// To2D function transform the given 1D board to a 2D board.
-// It accept the slice of Cell's, width of board, height of a board and return a matrix of Cell's.
-func To2D(board1D []Cell, h, w int) ([][]Cell, error) {
-	log.Info().Msg("Converting 1D board to 2D")
-	if len(board1D) == 0 {
-		return make([][]Cell, 0), fmt.Errorf("Board is empty(len() = %d)", len(board1D))
+func (b Board) RevealAll() error {
+	if len(b) == 0 {
+		return errors.New("board is empty")
 	}
-	if h <= 0 {
-		return make([][]Cell, 0), fmt.Errorf("Height( = %d) can't be <= 0", h)
-	}
-	if w <= 0 {
-		return make([][]Cell, 0), fmt.Errorf("Wight( = %d) can't be <= 0", w)
-	}
-	// Create result 2D board.
-	// The count of rows equals to h. The row length is equals to w.
-	board2D := make([][]Cell, h)
-
-	// This explanation is more for my better understanding.
-	/*
-		We have slice: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].
-		We want to create matrix with height 4 and width 3.
-		For this we need to cut our slice in to 4 small slice's with 3 elements in it
-
-		We iterate over empty matrix:
-
-
-				i = 0. First iteration.
-				Our empty row(i) accept three elements from slice:
-				Elements starts on slice[i * w] and ends on slice[(i+1) * w]
-				In out example: from slice[0 * 3 = 0] to slice[1 * 3 = 3]
-				We have slice: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].
-								^-----^
-				And like this our result now contain [1,2,3] in 0 row
-
-
-				i = 1. Second iteration
-				Get elements from slice[1 * 3 = 3] to slice[2 * 3 = 6]
-				matrix[i] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].
-									  ^-----^
-				Now matrix look like this:
-					[
-						[1, 2, 3],
-						[4, 5, 6],
-					]
-
-				i = 2.
-				matrix[2] = slice[2 * 3 = 6] : slice[3 * 3 = 9]
-				matrix[2] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].
-											   ^-----^
-	*/
-	for i := range h {
-		board2D[i] = board1D[i*w : (i+1)*w]
-	}
-	return board2D, nil
-}
-
-func RevealAll(board [][]Cell) ([][]Cell, error) {
-	if len(board) == 0 {
-		return make([][]Cell, 0), fmt.Errorf("Board is empty(len() = %d)", len(board))
-	}
-	log.Info().Msg("Revealing all cells")
-	for i, row := range board {
-		for j, cell := range row {
-			cell.Revealed = true
-			board[i][j] = cell
+	for i := range b {
+		for j := range b[i] {
+			b[i][j].Revealed = true
 		}
 	}
-	return board, nil
+	return nil
 }
 
-func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
-	if len(board) == 0 {
-		return [][]Cell{}, fmt.Errorf("Board can't be empty(len() = %d)", len(board))
+func (b Board) placeNumbers() error {
+	if len(b) == 0 {
+		return errors.New("board can't be empty")
 	}
 	// Another big comment for my understanding
 	/*
@@ -287,7 +253,7 @@ func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
 
 			Yeah, i don't know how to do this better, I just check all direction.
 	*/
-	for i, row := range board {
+	for i, row := range b {
 		for j, cell := range row {
 			if cell.IsMine {
 				// check position [i : j+1]
@@ -295,7 +261,7 @@ func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
 					■ X 1
 					■ ■ ■ */
 				if j != len(row)-1 {
-					board[i][j+1].MinesAround++
+					b[i][j+1].MinesAround++
 				}
 
 				// check position [i : j-1]
@@ -303,7 +269,7 @@ func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
 					1 X ■
 					■ ■ ■ */
 				if j != 0 {
-					board[i][j-1].MinesAround++
+					b[i][j-1].MinesAround++
 				}
 
 				// check position [i-1 : j]
@@ -311,14 +277,14 @@ func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
 					■ X ■
 					■ ■ ■ */
 				if i != 0 {
-					board[i-1][j].MinesAround++
+					b[i-1][j].MinesAround++
 
 					// check position [i-1 : j+1]
 					/*	■ ■ 1
 						■ X ■
 						■ ■ ■ */
 					if j != len(row)-1 {
-						board[i-1][j+1].MinesAround++
+						b[i-1][j+1].MinesAround++
 					}
 
 					// check position [i-1 : j-1]
@@ -326,7 +292,7 @@ func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
 						■ X ■
 						■ ■ ■ */
 					if j != 0 {
-						board[i-1][j-1].MinesAround++
+						b[i-1][j-1].MinesAround++
 					}
 				}
 
@@ -334,15 +300,15 @@ func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
 				/*	■ ■ ■
 					■ X ■
 					■ 1 ■ */
-				if i != len(board)-1 {
-					board[i+1][j].MinesAround++
+				if i != len(b)-1 {
+					b[i+1][j].MinesAround++
 
 					// check position [i+1 : j+1]
 					/*	■ ■ ■
 						■ X ■
 						■ ■ 1 */
 					if j != len(row)-1 {
-						board[i+1][j+1].MinesAround++
+						b[i+1][j+1].MinesAround++
 					}
 
 					// check position [i+1 : j-1]
@@ -350,19 +316,19 @@ func PlaceNumbers(board [][]Cell) ([][]Cell, error) {
 						■ X ■
 						1 ■ ■ */
 					if j != 0 {
-						board[i+1][j-1].MinesAround++
+						b[i+1][j-1].MinesAround++
 					}
 				}
 
 			}
 		}
 	}
-	for i, row := range board {
+	for i, row := range b {
 		for j, cell := range row {
 			if !cell.IsMine && cell.MinesAround == 0 {
-				board[i][j].Empty = true
+				b[i][j].Empty = true
 			}
 		}
 	}
-	return board, nil
+	return nil
 }
